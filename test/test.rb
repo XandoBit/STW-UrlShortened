0279b99767e9a66d2412366fd155b07467359fbe
Debora MPB <deborampb@gmail.com>
2014-10-23 10:24:06 +0100

    subiendo test

--- test/test.rb
+++ test/test.rb
@@ -12,3 +12,8 @@ def app
   Sinatra::Application
 end
 
+describe "Tests de la pagina raiz ('/') con metodo get" do
+    it "Carga de la web desde el servidor" do
+        get '/'
+        assert last_response.ok?
+    end
